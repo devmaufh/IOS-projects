e71aa9d983aa55283fe6ea1b730ca30bfb36eb1c
Devmaufh <mau1361317@gmail.com>
2019-08-20 08:41:34 -0500

    Basics swift

---
+++ Swift-basics/aspectos basicos/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+let number = 10
+var str = "Hello world"
+print(str)
+print("Constante \(number)", separator:"")
+
+
+/*  Tuplas  */
+var cities = ("Mexico","USA", "Canada")
+
+print(cities.0)
+print(cities)
+
+var cities2 = (guanajuato:"Celaya", queretaro: "Queretaro")
+print(cities2)
+
+
+/*  Arrays  */
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+for number in numbers {
+    print("Numero: \(number) ")
+}
+
+//Array inicializacion
+var numbers2 = [String]()
+numbers2.append("1")
+numbers2.append("2")
+numbers2.append("3")
+
+print(numbers2)
+
+/*  Arrays  */
+
