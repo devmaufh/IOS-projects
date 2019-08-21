f580d8688a66c86fd0c528b57d64c32b956567ef
Devmaufh <mau1361317@gmail.com>
2019-08-21 08:51:29 -0500

    Flujos de control

--- Swift-basics/aspectos basicos/MyPlayground.playground/Contents.swift
+++ Swift-basics/aspectos basicos/MyPlayground.playground/Contents.swift
@@ -4,20 +4,24 @@ var str = "Hello world"
 print(str)
 print("Constante \(number)", separator:"")
 
+let val :Int = 10
 
 /*  Tuplas  */
-var cities = ("Mexico","USA", "Canada")
+var cities = ("Mexico",1,2.0, "Canada")
 
 print(cities.0)
-print(cities)
+print(cities.3)
 
-var cities2 = (guanajuato:"Celaya", queretaro: "Queretaro")
-print(cities2)
+var cities2 = (guanajuato:"Celaya", queretaro: "Queretaro",numero:99)
+print(cities2.numero)
 
 
 /*  Arrays  */
 
 let numbers = [1,2,3,4,5,6,7,8,9]
+var x = [1,2,3]
+x.append(9999)
+
 for number in numbers {
     print("Numero: \(number) ")
 }
@@ -30,5 +34,8 @@ numbers2.append("3")
 
 print(numbers2)
 
-/*  Arrays  */
+/*  Dicccionarios  */
 
+var dictionary = ["name" : "Mauricio", "last": "Flores"]
+dictionary["second_name"] = "Juan daniel"
+print(dictionary["name"])
